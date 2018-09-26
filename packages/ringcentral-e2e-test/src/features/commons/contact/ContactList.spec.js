cc959edb5bec2e0643d4e03cd7fdcb5c2d8f521b
jackzong <jinzhanglin@hotmail.com>
2018-09-26 21:23:08 +0800

    update

--- packages/ringcentral-e2e-test/src/features/commons/contact/ContactList.spec.js
+++ packages/ringcentral-e2e-test/src/features/commons/contact/ContactList.spec.js
@@ -8,7 +8,7 @@ import NavigateTo from '../../../steps/commons/navigateTo';
 import NavigationBar from 'ringcentral-widgets/components/NavigationBar';
 describe('Contact list automated test', () => {
   test({
-    title: 'Contact list with account ${username}',
+    title: 'Contact list with account ${username}, search content',
     tags: [['widgets']],
     options: [
       { username: '+18552085154*101',
@@ -27,10 +27,34 @@ describe('Contact list automated test', () => {
       NavigateTo,
     )(context);
     await process.exec();
-    const fromNumber = await $(app).getText(option.selector);
-    expect(fromNumber).toBe(option.title);
-    await $(app).timeout(2000);
-    app.$('input')
-    await $(app).screenshot({ path: 'rd.png'});
-  })
+    const contactNav = await page.$("div[title='Contacts']");
+    await contactNav.click();
+    await $(app).timeout(1000);
+    const search = await page.$("input[name='search']");
+    await search.focus();
+    await search.type('FirstName', { delay: 200 });
+    await $(app).timeout(1000);
+    await search.type('abc', { delay: 100 });
+    await $(app).timeout(500);
+    const title = await page.$eval('._-ringcentral-widgets-components-ContactList-_styles_noContacts_avCq5', e => {
+      return e.innerHTML
+    });
+    expect(title).toEqual('No records found.');
+  });
+  test({
+    title: 'Contact list filter',
+    levels: ['p0'],
+    tags: [['widgets']],
+    options: [{
+
+    }]
+  }, async({ option, isVirtual }) => {
+    await $(app).screenshot({ path: 'f.png' });
+    const contactNav = await page.$("div[title='Contacts']");
+    await contactNav.click();
+    await $(app).screenshot({ path: 'c.png' });
+    const filterBtn = page.$('._-ringcentral-widgets-components-ContactSourceFilter-_styles_contactSourceFilter_1YaIl _-ringcentral-widgets-components-ContactsView-_styles_actionButton_3aSnr');
+    await filterBtn.click();
+    $(app).screenshot({ path: 'f.png' });
+  });
 })
